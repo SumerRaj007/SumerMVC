b2c2b56c7adfffa89f3ca6fb06d59b3249be36c3
SumerRaj007 <34752270+SumerRaj007@users.noreply.github.com>
2020-06-13 05:27:30 -0700

    Add files via upload

---
+++ CutomersController.cs
@@ -0,0 +1,40 @@
+﻿using SumerRaj.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SumerRaj.Controllers
+{
+    public class CutomersController : Controller
+    {
+        // GET: Cutomers
+        public ActionResult Index()
+        {
+            var getcustomers = GetCustomers();
+            return View(getcustomers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{ Id=1,Name="Sumer"},
+                new Customer{Id=2,Name="Nitin"},
+                new Customer{Id=3,Name="Bhanu"},
+                new Customer{Id=4,Name="Suraj"},
+                new Customer{Id=5,Name="Aditya"}
+           };
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customers == null)
+                return HttpNotFound();
+            return View(customers);
+        }
+    }
+}
---
+++ MoviesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SumerRaj.Models;
+using SumerRaj.ViewModel;
+
+namespace SumerRaj.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var getMovie = GetMovies();
+            return View(getMovie);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //return Content("Hello world");
+            // return HttpNotFound();
+            //return new EmptyResult();
+            // return RedirectToAction("Index", "Home");
+            //return RedirectToAction("Index", "Home", new { page = 1, sortby = "Name" });
+            //ViewData["RandomMovie"] = movie;
+            //ViewBag.Movie = movie;
+            var customers = new List<Customer>
+            {
+               new Customer{ Name="Customer 1"},
+               new Customer{ Name="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model
+            return View(viewModel);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1,Name="Fast&Furious"},
+                new Movie{Id=2,Name="MIssion Imposible"}
+            };
+        }
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("Id= "  +id);
+        //}
+
+        //public ActionResult Index(int? pageIndex,string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrEmpty(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
+        //[Route("Movies/ByReleaseDate/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year,int month)
+        //{
+        //    return Content(year+ "/" + month);
+        //}
+
+    }
+}
